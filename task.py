c187f51133f68a516519d3837610614f10557d7d
0wenmack <122229266+0wenmack@users.noreply.github.com>
2025-03-22 20:06:01 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,5 +1,8 @@
+import random
+
 # TARGET ARRAY
 A = [5, -3, -10, 4, -1, -2, 1, 7, -5, 8]
+TEXT = "MODE:\nType 1 for Manual Array input\nType 2 for Random Generated Array\nType 0 for predefined hardcoded Array\n\n > "
 
 def sum_negative_between_max_min(A):
     if len(A) < 3:
@@ -16,7 +19,22 @@ def sum_negative_between_max_min(A):
     negative_elements = [x for x in A[start:end] if x < 0]
     return sum(negative_elements)
 
+def generate_array(N, lower_bound=-10, upper_bound=10):
+    return [random.randint(lower_bound, upper_bound) for _ in range(N)]
+
 if __name__ == "__main__":
-	# EXAMPLE
-	result = sum_negative_between_max_min(A)
-	print("Negative elements sum between min и max:", result)
+    choice = int(input(TEXT))
+    if (choice == 1):
+        A = [int(i) for i in (input("Enter numbers separated by a space:")).split()]
+    elif (choice == 2):
+        N = int(input("Array size(int): "))
+        A = generate_array(N)
+        print("Generated Array:", A)
+    elif (choice == 0):
+        pass
+    else:
+        print("Not a valid option")
+    
+    # EXAMPLE
+    result = sum_negative_between_max_min(A)
+    print("Negative elements sum between min и max:", result)
